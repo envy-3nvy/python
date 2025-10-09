666a3b2ef9b26910612a4a9f21aab8699f23da5e
envy-3nvy <asats79@gmail.com>
2025-10-09 14:21:01 +0300

    Add files via upload

--- h01.py
+++ h01.py
@@ -1,1 +1,32 @@
-print ("tere maailm!")
+import turtle
+
+#liigu kuhugi
+turtle.penup()
+turtle.goto(-400,200)
+turtle.pendown()
+#kolmnurk
+for i in range(3): #tsükkel 3x
+    turtle.fd(200)
+    turtle.lt(120)
+#liigu palun konn
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+#ruut
+for i in range(4):
+    turtle.fd(150)
+    turtle.lt(90)
+
+turtle.penup()
+turtle.goto(200,-200)
+turtle.pendown()
+#A täht
+turtle.fd(130)
+turtle.rt(45)
+turtle.fd(50)
+turtle.bk(150)
+turtle.rt(85)
+turtle.fd(150)
+
+
+turtle.done()
